0528ed8e0c0e7267a9b8da4e772f0a7642175240
cjungm <48304104+cjungm@users.noreply.github.com>
2019-10-16 17:48:56 +0900

    Add files via upload

---
+++ JAVA/Baekjoon/src/Queue/Base.java
@@ -0,0 +1,100 @@
+package Queue;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Base {
+	static int head=0;
+	public static void main(String[] args) {
+//		정수를 저장하는 큐를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+//		명령은 총 여섯 가지이다.
+//		push X: 정수 X를 큐에 넣는 연산이다.
+//		pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//		size: 큐에 들어있는 정수의 개수를 출력한다.
+//		empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
+//		front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+//		back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+		int n=0;
+		String msg="";
+		Scanner scanner = new Scanner(System.in);
+		n= scanner.nextInt();
+		int i=0;
+		ArrayList<Integer>stack = new ArrayList<>();
+
+		while(i<=n) {
+			msg = scanner.nextLine();
+			String [] command =msg.split(" ");
+			switch(command[0]) {
+			case "push":
+				int x=Integer.parseInt(command[1]);
+				push(stack,x);
+				break;
+			case "pop":
+				pop(stack);
+				break;
+			case "size":
+				size(stack);
+				break;
+			case "empty":
+				empty(stack);
+				break;
+			case "front":
+				front(stack);
+				break;
+			case "back":
+				back(stack);
+				break;
+			}
+			i++;
+		}
+		scanner.close();
+	}
+
+	 static void back(ArrayList<Integer> stack) {
+		 if(head>0) {
+			 System.out.println(stack.get(head-1));
+		}
+		else {
+			System.out.println(-1);
+		}		
+		
+	}
+
+	static void front(ArrayList<Integer> stack) {
+		 if(head>0) {
+			 System.out.println(stack.get(0));
+		}
+		else {
+			System.out.println(-1);
+		}		
+	}
+
+	static void empty(ArrayList<Integer> stack) {
+		 if(stack.isEmpty()) {
+			 System.out.println(1);
+		 }else {
+			 System.out.println(0);
+		 }
+	}
+
+	static void size(ArrayList<Integer> stack) {
+		 System.out.println(head);
+	}
+
+	static void pop(ArrayList<Integer> stack) {
+		if(head>0) {
+			 System.out.println(stack.get(0));
+			 stack.remove(0);
+			 head--;
+		}
+		else {
+			System.out.println(-1);
+		}
+	}
+
+	static void push(ArrayList<Integer> stack, int x) {
+		stack.add(x);
+		head++;
+	}
+
+}
